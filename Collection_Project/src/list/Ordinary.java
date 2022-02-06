5208656541118b1234d853ca4012cb3f43054d89
undo <undoworks4649@gmail.com>
2022-02-06 21:45:23 +0900

    02/06 21:45

--- Collection_Project/src/list/Ordinary.java
+++ Collection_Project/src/list/Ordinary.java
@@ -8,24 +8,34 @@ import java.util.List;
  * This class uses the main methods of the Collection interface.
  */
 public class Ordinary {
-
 	public static void main(String[] args) {
 		List<String> list = new ArrayList<String>();
+		
+		//要素が含まれていないか確認
+		System.out.println(list.isEmpty()); //true
 		
 		//コレクションへ追加
 		list.add("あいうえお");
 		list.add("かきくけこ");
 		list.add("さしすせそ");
 		
+		//要素数の取得(int)
+		System.out.println(list.size()); //3
+		
 		//要素の取得
 		System.out.println(list.get(0)); //あいうえお
 		
-		//要素を含むか調べる
+		//要素を含むか調べる 
+		//複数は「boolean containsAll(Collection<?> c)」
 		System.out.println(list.contains("あいうえお")); //true
-		System.out.println(list.contains("かきくけこ")); //false 
+		System.out.println(list.contains("たちつてと")); //false 
 		
-		//
+		//要素がすべて格納された配列を取得
+		Object[] objectArray = list.toArray();
+		System.out.println(objectArray[0]); //あいうえお
+		System.out.println(objectArray[1]); //かきくけこ
+		
+		
 		
 	}
-
 }
